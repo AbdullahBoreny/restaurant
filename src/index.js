1945d85a8b632b90a24901ffdc866d1ab9278e28
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-02-09 10:22:57 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import Data from "./data/data.xml";
-import Notes from "./data/data.csv";
-import "./styles/index.css";
-const table = document.querySelector("table");
-for (let i = 0; i < table.rows.length; i++) {
-  // Loop through rows
-  let row = table.rows[i];
-  for (let j = 0; j < row.cells.length; j++) {
-    console.log(Notes[i][j])
-    row.cells[j].textContent = Notes[i][j];
-  }
-}
+console.log("hello");
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,24 @@
+// webpack.config.js
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-let htmlPageNames = ["about", "letter"];
-let multipleHtmlPlugins = htmlPageNames.map((name) => {
-  return new HtmlWebpackPlugin({
-    template: `./src/templates/${name}.html`,
-    filename: `${name}.html`,
-    chunks: [`${name}`], // respective JS files
-  });
-});
 
 export default {
   mode: "development",
-  entry: { main: "./src/index.js", about: "./src/about.js", letter: "./src/letter.js" },
+  entry: "./src/index.js",
   output: {
-    filename: "[name].js",
+    filename: "bundle.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: [
-      "./src/templates/index.html",
-      "./src/templates/about.html",
-      "./src/templates/letter.html",
-    ],
+    watchFiles: ["./src/templates/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/templates/index.html",
-      chunks: ["main"],
+      template: "./src/templates/template.html",
     }),
-  ].concat(multipleHtmlPlugins),
-
+  ],
   module: {
     rules: [
       {
@@ -45,18 +32,6 @@ export default {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
       },
     ],
   },
